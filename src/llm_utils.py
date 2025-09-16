4a1a18efaa15fc5018a10283684df89eeed2e6eb
orin <orin@imperva.com>
2025-09-16 13:02:41 +0300

    support titan model. Add token count to API

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -1,21 +1,21 @@
 import json
 import logging
 import os
-from typing import Optional
 
 from boto3.session import Session
 from botocore.config import Config
 
 
 def invoke_llm(
-    system_prompt: Optional[str], user_prompt: str, model_id: str, session: Session
-) -> str:
+    system_prompt: str, user_prompt: str, model_id: str, session: Session
+) -> dict:
     logging.info(f"Going to invoke LLM. Model ID: {model_id}")
-    prompt = _format_model_body(user_prompt, system_prompt, model_id)
+    prompt = _format_model_body(system_prompt, user_prompt, model_id)
     response_json = _invoke_bedrock_model(prompt, model_id, session)
     response_text = _get_response_content(response_json, model_id)
+    usage = _get_response_usage(response_json, model_id)
     logging.info(f"Got response from LLM. Response length: {len(response_text)}")
-    return response_text
+    return {"content": response_text, "usage": usage}
 
 
 def _invoke_bedrock_model(prompt_body: dict, model_id: str, session: Session) -> dict:
@@ -32,11 +32,7 @@ def _invoke_bedrock_model(prompt_body: dict, model_id: str, session: Session) ->
     return json.loads(response.get("body").read())
 
 
-def _format_model_body(
-    prompt: str, system_prompt: Optional[str], model_id: str
-) -> dict:
-    if system_prompt is None:
-        system_prompt = "You are a SQL generator helper"
+def _format_model_body(system_prompt: str, user_prompt: str, model_id: str) -> dict:
     if "claude" in model_id:
         body = {
             "anthropic_version": "bedrock-2023-05-31",
@@ -44,7 +40,7 @@ def _format_model_body(
             "messages": [
                 {
                     "role": "user",
-                    "content": prompt,
+                    "content": user_prompt,
                 }
             ],
             "max_tokens": 2000,
@@ -54,10 +50,12 @@ def _format_model_body(
         body = {
             "messages": [
                 {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt},
+                {"role": "user", "content": user_prompt},
             ],
             "n": 1,
         }
+    elif "titan" in model_id:
+        body = {"inputText": f"{system_prompt}\n\n{user_prompt}"}
     else:
         raise ValueError(f"Unknown model_id: {model_id}")
     return body
@@ -68,5 +66,27 @@ def _get_response_content(response_json: dict, model_id: str) -> str:
         return response_json["content"][0]["text"]
     elif "jamba" in model_id:
         return response_json["choices"][0]["message"]["content"]
+    elif "titan" in model_id:
+        return response_json["results"][0]["outputText"]
+    else:
+        raise ValueError(f"Unknown model_id: {model_id}")
+
+
+def _get_response_usage(response_json: dict, model_id: str) -> dict:
+    if "claude" in model_id:
+        return {
+            "input_tokens": response_json["usage"]["input_tokens"],
+            "output_tokens": response_json["usage"]["output_tokens"],
+        }
+    elif "jamba" in model_id:
+        return {
+            "input_tokens": response_json["usage"]["prompt_tokens"],
+            "output_tokens": response_json["usage"]["completion_tokens"],
+        }
+    elif "titan" in model_id:
+        return {
+            "input_tokens": response_json["inputTextTokenCount"],
+            "output_tokens": response_json["results"][0]["tokenCount"],
+        }
     else:
         raise ValueError(f"Unknown model_id: {model_id}")
