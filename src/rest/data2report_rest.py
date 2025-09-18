576b151bd78fc4bdb0ab6a7b92d93363d08b4461
orin <orin@imperva.com>
2025-09-18 18:57:20 +0300

    add initial UI

--- src/rest/data2report_rest.py
+++ src/rest/data2report_rest.py
@@ -1,21 +1,50 @@
+import json
 import logging
 import os
+import tempfile
 from logging.config import fileConfig
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
+from data2report import run_report
+from utils import get_reports_folder, init_env_from_file
 
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET"])
-def _main():
-    return _ping()
+app = Flask(
+    __name__,
+    template_folder=os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "templates"
+    ),
+)
 
 
 @app.route("/ping", methods=["GET"])
 def _ping():
     return jsonify({"status": "UP"}), 200
+
+
+@app.route("/", methods=["GET"])
+def _report_form():
+    return render_template("run_report.html")
+
+
+@app.route("/report", methods=["POST"])
+def _report():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files["file"]
+    config = request.form["config"]
+    max_records = request.form.get("max_records")
+    force = request.form.get("force", "false").lower() == "true"
+    print(f"Config: {config}")
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        file.save(tmp_file.name)
+        result = run_report(
+            input_file=tmp_file.name,
+            configuration=json.loads(config),
+            max_records=max_records,
+            force=force,
+        )
+    return jsonify(result)
 
 
 def _init_logging():
@@ -27,5 +56,8 @@ if __name__ == "__main__":
     _init_logging()
     logging.getLogger("werkzeug").setLevel("WARNING")
     port = os.environ.get("APP_PORT", 5000)
-    logging.info(f"Going to start the app. Port: {port}")
-    app.run(host="0.0.0.0", port=port)
+    logging.info(
+        f"Going to start the app. Port: {port}. Reports folder: {get_reports_folder()}"
+    )
+    init_env_from_file()
+    app.run(host="0.0.0.0", port=os.getenv("APP_PORT", port))
